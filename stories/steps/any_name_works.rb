ffb46d62c2a7891600782001517917119e073d63
Kyle Hargraves <philodespotos@gmail.com>
2008-04-06 11:36:48 -0500

    Make it more obvious that steps/*.rb filenames are irrelevant

--- stories/steps/any_name_works.rb
+++ stories/steps/any_name_works.rb
@@ -1,3 +1,7 @@
+# As long as a step group is loaded from your helper.rb,
+# the filename for it is irrelevant. Just make sure the group
+# names specified via steps_for() match up.
+
 steps_for('some other name') do
   Given('that the step file name does not match the step group') do end
   Given('that the helper file loaded the file the step group is in') do end
